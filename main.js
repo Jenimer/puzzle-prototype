efa1083952e9aa0ab0d5617ba0a5050e53b822a4
Jen Broadhead <jeniffer.broadhead@gmail.com>
2019-11-26 13:04:45 -0700

    got some of the logic working

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ button2 = document.getElementById('btn2')
 button3 = document.getElementById('btn3')
 button4 = document.getElementById('btn4')
 displayed = document.getElementById('lightValues')
-//displayed.style.display = 'none'
+displayed.style.display = 'none'
 
 allLights = [
   light1,
@@ -47,10 +47,10 @@ clicky = (light) => {
 
 count = () => {
   let solutions = [
-    [1, 2, 3, 4],
-    [2, 3, 4, 1],
-    [3, 4, 1, 2],
-    [4, 1, 2, 3]
+   1234,
+   2341,
+   3412,
+   4123
   ]
 
   //use this to compare length
@@ -69,27 +69,63 @@ count = () => {
     let number = parseInt(n);
     convertedNumbers.push(number);
   });
-
-  console.log(convertedNumbers);
+  let thing = convertedNumbers.join('');
+  console.log(thing);
+  
   //function that runs all comparisons, renders a result and clears
   //everything to be run until program is closed
-  if (list.length == 4 && solutions[0] == convertedNumbers) {
-    setTimeout(() => {
-      revGlow();
-      i = 0;
-      while (list.length != 0) {
-        more = document.getElementsByTagName('li')[0];
-        more.parentNode.removeChild(more);
-        i++;
-        console.log(more)
-      }
-    }, 500);
+  if (list.length == 4) {
+    if(thing == solutions[0] || 
+       thing == solutions[1] || 
+       thing == solutions[2] || 
+       thing == solutions[3]) {
+
+      setTimeout(() => {
+        right(allLights)
+        clearList(list);
+      }, 500);
+      setTimeout(() => {
+        revGlow()
+      }, 1200)
+    }else{
+      setTimeout(() => {
+        wrong(allLights);
+        clearList(list);
+      }, 500);
+      setTimeout(() => {
+        revGlow()
+      }, 1200)
+    }
   }
 }
 
-glow = (light) => {
-  light.style.background = 'lime'
+clearList = (list) => {
+  i = 0;
+  while (list.length != 0) {
+    more = document.getElementsByTagName('li')[0];
+    more.parentNode.removeChild(more);
+    i++;
+    console.log(more)
+  }
 }
+
+
+glow = (light) => {
+  light.style.background = '#00d9ff'
+}
+
+right = (alllight) => {
+  alllight.map(light => {
+    light.style.background = 'lime'
+  })
+}
+
+wrong = (alllight) => {
+  alllight.map(light => {
+    light.style.background = 'red'
+  })
+}
+
 
 revGlow = () => {
   allLights.map(l => {
